a321778157f7b1cfaebf5903fed67f97c77717bc
Ngo Thi Linh <74759189+ngothilinh20187179@users.noreply.github.com>
2021-06-08 14:35:44 +0700

    Update FPTClient2.cpp

--- Samples/FTP/FPTClient2.cpp
+++ Samples/FTP/FPTClient2.cpp
@@ -11,20 +11,20 @@ int main()
 {
     WSADATA wsa;
     WSAStartup(MAKEWORD(2, 2), &wsa);
-  
+    
     SOCKADDR_IN addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     addr.sin_port = htons(21);
-  
+    
     SOCKET controlSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     connect(controlSocket, (SOCKADDR*)&addr, sizeof(addr));
-  
+    
     char buf[2048];
     int ret = recv(controlSocket, buf, sizeof(buf), 0);
     buf[ret] = 0;
     puts(buf);
-  
+    
     // Dang nhap    
     char msg[256];
     char user[32], pass[32];
@@ -32,21 +32,21 @@ int main()
     {
         printf("Nhap username va password: ");
         scanf("%s%s", user, pass);
-      
+        
         // Gui lenh USER        
         sprintf(msg, "USER %s\n", user);
         send(controlSocket, msg, strlen(msg), 0);
         ret = recv(controlSocket, buf, sizeof(buf), 0);
         buf[ret] = 0;
         puts(buf);
-      
+        
         // Gui lenh PASS        
         sprintf(msg, "PASS %s\n", pass);
         send(controlSocket, msg, strlen(msg), 0);
         ret = recv(controlSocket, buf, sizeof(buf), 0);
         buf[ret] = 0;
         puts(buf);
-      
+        
         // Kiem tra ket qua dang nhap        
         if (strncmp(buf, "230", 3) == 0)
         {
@@ -57,14 +57,14 @@ int main()
             puts("Dang nhap that bai!");
         }
     }
-  
+    
     // Gui lenh PASV    
     strcpy(msg, "PASV\n");
     send(controlSocket, msg, strlen(msg), 0);
     ret = recv(controlSocket, buf, sizeof(buf), 0);
     buf[ret] = 0;
     puts(buf);
-  
+    
     // Tach ra dia chi IP va cong    
     char* p1 = strchr(buf, '(');
     char* p2 = strchr(buf, ')');
@@ -79,7 +79,7 @@ int main()
         i++;
         p = strtok(NULL, ",");
     }
-  
+    
     // Tao ket noi truyen du lieu    
     SOCKADDR_IN addrData;
     addrData.sin_family = AF_INET;
@@ -87,21 +87,21 @@ int main()
     addrData.sin_port = htons(arrAdd[4] * 256 + arrAdd[5]);
     SOCKET dataSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     connect(dataSocket, (SOCKADDR*)&addrData, sizeof(addrData));
-  
+    
     // Gui lenh TYPE I    
     strcpy(msg, "TYPE I\n");
     send(controlSocket, msg, strlen(msg), 0);
     ret = recv(controlSocket, buf, sizeof(buf), 0);
     buf[ret] = 0;
     puts(buf);
-  
+    
     // Gui lenh STOR    
     strcpy(msg, "STOR upload.jpg\n");
     send(controlSocket, msg, strlen(msg), 0);
     ret = recv(controlSocket, buf, sizeof(buf), 0);
     buf[ret] = 0;
     puts(buf);
-  
+    
     // Upload du lieu qua kenh du lieu    
     printf("\n------\n");
     FILE* f = fopen("C:\\test\\test.jpg", "rb");
@@ -116,14 +116,16 @@ int main()
     printf("\n------\n");
     fclose(f);
     closesocket(dataSocket);
-  
+    ret = recv(controlSocket, buf, sizeof(buf), 0);
+    buf[ret] = 0;
+    puts(buf);
+    
     // Gui lenh QUIT    
     strcpy(msg, "QUIT\n");
     send(controlSocket, msg, strlen(msg), 0);
     ret = recv(controlSocket, buf, sizeof(buf), 0);
     buf[ret] = 0;
     puts(buf);
-  
     closesocket(controlSocket);
     WSACleanup();
 }
